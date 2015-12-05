26e967fbb36158c41458071128e40e7e834e7724
Matthew Bentley <matthew.t.bentley@gmail.com>
2015-12-05 15:00:11 -0500

    It mostly sort of works. There is still a problem that it won't work with more than ~336 workers before getting "Too many open files" errors.

--- mqtt_locust.py
+++ mqtt_locust.py
@@ -10,15 +10,18 @@ from locust import events
 
 class MQTTClient(mqtt.Client):
 
-    def __getattr__(self, name):
-        attr = mqtt.Client.__getattr__(self, name)
+    def sendMessage(self, topic, message):
+        print(topic, message)
+
+    def __getattribute__(self, name):
+        attr = mqtt.Client.__getattribute__(self, name)
         if not callable(attr):
             return attr
 
         def wrapper(*args, **kwargs):
             start_time = time.time()
             try:
-                resule = attr(*args, **kwargs)
+                result = attr(*args, **kwargs)
             except Exception as e:
                 total_time = int((time.time() - start_time) * 1000)
                 events.request_failure.fire(
@@ -33,14 +36,20 @@ class MQTTClient(mqtt.Client):
                         response_time=total_time, response_length=0
                         )
 
-        return wrapper
+        if name == 'publish':
+            return wrapper
+        else:
+            return attr
 
 
 class MQTTLocust(Locust):
 
     def __init__(self, *args, **kwargs):
         super(Locust, self).__init__(*args, **kwargs)
+        if self.host is None:
+            raise LocustError("You must specify a host")
         self.client = MQTTClient()
+        self.client.connect(self.host)
 
 
 class ExampleMQTTClientBehavior(TaskSet):
