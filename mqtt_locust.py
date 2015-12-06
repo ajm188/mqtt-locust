30afb2db8cc61464bddf66b900e44efa45874aa1
Andrew Mason <andrew@fixedpoint.xyz>
2015-12-06 10:59:41 -0500

    remove stupid example and make sure that the client loop starts

--- mqtt_locust.py
+++ mqtt_locust.py
@@ -54,42 +54,4 @@ class MQTTLocust(Locust):
             raise LocustError("You must specify a host")
         self.client = MQTTClient()
         self.client.connect(self.host)
-
-
-class ExampleMQTTClientBehavior(TaskSet):
-
-    @task(1)
-    def pub_to_set_config(self):
-        self.client.single(
-            self.topic,
-            payload=self.payload(),
-            qos=self.qos,
-            retain=self.retain,
-            hostname=self.hostname,
-            port=self.port,
-        )
-
-
-class ExampleMQTTLoadTester(MQTTLocust):
-
-    topic = 'lamp/set_config'
-    qos = 1
-    retain = False
-    hostname = 'my.mqtt.host.sucks'
-    port = 1883
-
-    min_wait = 5
-    max_wait = 500
-
-    def payload(self):
-        payload = {
-            'on': random.choice(['true', 'false']),
-            'color': {
-                'h': random.random(),
-                's': random.random(),
-            },
-            'brightness': random.random(),
-        }
-        return payload
-
-    task_set = ExampleMQTTClientBehavior
+        self.client.loop_start()
