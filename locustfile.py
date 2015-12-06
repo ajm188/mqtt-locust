f0f10c686c0d69bdf0a15c42a32995d1b458b783
Andrew Mason <andrew@fixedpoint.xyz>
2015-12-06 10:58:41 -0500

    add a dynamic payload

--- locustfile.py
+++ locustfile.py
@@ -1,11 +1,27 @@
+import json
+import random
+
 from locust import TaskSet, task
 
 from mqtt_locust import MQTTLocust
 
+
 class MyTaskSet(TaskSet):
     @task(1)
     def test(self):
-        self.client.publish('/topic', 'Message')
+        self.client.publish('lamp/set_config', self.payload())
+
+    def payload(self):
+        payload = {
+            'on': random.choice(['true', 'false']),
+            'color': {
+                'h': random.random(),
+                's': random.random(),
+            },
+            'brightness': random.random(),
+        }
+        return json.dumps(payload)
+
 
 class MyLocust(MQTTLocust):
     task_set = MyTaskSet
